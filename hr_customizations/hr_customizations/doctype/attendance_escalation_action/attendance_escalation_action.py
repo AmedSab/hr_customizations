b5d1d7a7e06490b1cfe09caf19676c6fc26326bc
frappe_user <frappe@DESKTOP-E53DG33.localdomain>
2026-03-30 10:38:34 +0300

    Feat: Add deduct_salary and handled leave deduction cancelation

--- hr_customizations/hr_customizations/doctype/attendance_escalation_action/attendance_escalation_action.py
+++ hr_customizations/hr_customizations/doctype/attendance_escalation_action/attendance_escalation_action.py
@@ -30,65 +30,77 @@ class AttendanceEscalationAction(Document):
     def on_submit(self):
         if self.action == "Deduct Leave Day":
             self.deduct_leave()
+        elif self.action == "Deduct Salary Day":
+            self.deduct_salary()
+    
+    def before_cancel(self):
+        entries = frappe.get_all(
+            "Leave Ledger Entry",
+            filters={
+                "transaction_name": self.name,
+                "transaction_type": self.doctype,
+                "docstatus": 1
+            },
+            pluck="name"
+        )
+
+        for entry in entries:
+            frappe.db.set_value("Leave Ledger Entry", entry.name, "is_expired", True)
+            frappe.db.commit()
+            doc = frappe.get_doc("Leave Ledger Entry", entry)
+            doc.cancel()
 
     
     def deduct_leave(self):
         if not self.employee or not self.leave_type_for_deduction:
             frappe.throw("Employee and Leave Type are required")
 
-        leave_date = get_next_valid_day(self.employee, nowdate())
+        action_date = nowdate()
 
+        leave_ledger_entry_doc = frappe.new_doc("Leave Ledger Entry")
+        leave_ledger_entry_doc.update({
+            'employee': self.employee,
+            'leave_type': self.leave_type_for_deduction,
+            'from_date': action_date,
+            'to_date': action_date,
+            'leaves': -1,
+            'transaction_type': self.doctype,
+            'transaction_name': self.name,
+        })
+        leave_ledger_entry_doc.submit()
         
-        leave = frappe.get_doc({
-        "doctype": "Leave Application",
-        "employee": self.employee,
-        "leave_type": self.leave_type_for_deduction,
-        "from_date":leave_date,
-        "to_date": leave_date,
-        "description": f"Auto deduction from escalation {self.name}"
-    })
-
-        leave.insert(ignore_permissions=True)
-        leave.submit()    
-
-        # allocation = frappe.get_all("Leave Allocation", 
-        #                 filters={'leave_type': ['=', self.leave_type_for_deduction], 'docstatus': ['=', 1]
-        #                          , 'employee': ['=', self.employee]
-        #                          }, 
-        #                 fields=["name", "employee", "new_leaves_allocated", "total_leaves_allocated"],
-        #                 limit=1
-        #                 )
-        
-        # if allocation:
-        #     frappe.db.set_value(
-        #         "Leave Allocation",
-        #         allocation[0].name,
-        #         "total_leaves_allocated",
-        #         allocation[0].total_leaves_allocated - 1
-        #     )
 
         frappe.logger().info(f"Leave deducted for {self.employee}")
 
 
-        def get_next_valid_day(self, employee, date):
-            holiday_list = frappe.db.get_value("Employee", employee, "holiday_list")
+    def deduct_salary(self):
+        if not self.employee or not self.salary_component_for_deduction:
+            frappe.throw("Employee and Salary Component are required")
 
-            while True:
-                is_holiday = frappe.db.exists("Holiday", {
-                    "parent": holiday_list,
-                    "holiday_date": date
-                })
+        base_salary = frappe.db.get_value("Salary Structure Assignment", 
+        {"employee": self.employee, "docstatus": 1}, "base")
 
-                leave_exists = frappe.db.exists("Leave Application", {
-                    "employee": employee,
-                    "leave_type": self.leave_type_for_deduction,
-                    "docstatus": 1,
-                    "from_date": ["<=", date],
-                    "to_date": [">=", date]
-                })
+        if not base_salary:
+            frappe.throw(f"No active Salary Structure Assignment found for {self.employee}")
 
-                if not is_holiday and not leave_exists:
-                    return date
+        total_working_days = 30
 
-                date = add_days(date, 1)
+        amount_to_deduct = (base_salary) / total_working_days
+
+        additional_salary = frappe.get_doc({
+            "doctype": "Additional Salary",
+            "employee": self.employee,
+            "salary_component": self.salary_component_for_deduction,
+            "amount": amount_to_deduct,
+            "payroll_date": nowdate(),
+            "ref_doctype": self.doctype,
+            "ref_docname": self.name,
+            "remarks": f"Auto deduction from escalation {self.name}"
+        })
+
+        additional_salary.insert(ignore_permissions=True)
+        additional_salary.submit()
+
+        frappe.logger().info(f"Salary deducted for {self.employee}")
+
 
